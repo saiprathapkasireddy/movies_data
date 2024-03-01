cc2084c63429d89923733ebdaf6664c8bd1da302
Sai Prathap <saiprathapkasireddy@gmail.com>
2024-02-29 20:25:31 -0500

    movies details

---
+++ movieData.js
@@ -0,0 +1,49 @@
+const favouritesMovies = {
+    Matrix: {
+      imdbRating: 8.3,
+      actors: ["Keanu Reeves", "Carrie-Anniee"],
+      oscarNominations: 2,
+      genre: ["sci-fi", "adventure"],
+      totalEarnings: "$680M",
+    },
+    FightClub: {
+      imdbRating: 8.8,
+      actors: ["Edward Norton", "Brad Pitt"],
+      oscarNominations: 6,
+      genre: ["thriller", "drama"],
+      totalEarnings: "$350M",
+    },
+    Inception: {
+      imdbRating: 8.3,
+      actors: ["Tom Hardy", "Leonardo Dicaprio"],
+      oscarNominations: 12,
+      genre: ["sci-fi", "adventure"],
+      totalEarnings: "$870M",
+    },
+    "The Dark Knight": {
+      imdbRating: 8.9,
+      actors: ["Christian Bale", "Heath Ledger"],
+      oscarNominations: 12,
+  
+      genre: ["thriller"],
+      totalEarnings: "$744M",
+    },
+    "Pulp Fiction": {
+      imdbRating: 8.3,
+  
+      actors: ["Sameul L. Jackson", "Bruce Willis"],
+      oscarNominations: 7,
+      genre: ["drama", "crime"],
+      totalEarnings: "$455M",
+    },
+    Titanic: {
+      imdbRating: 8.3,
+      actors: ["Leonardo Dicaprio", "Kate Winslet"],
+      oscarNominations: 13,
+      genre: ["drama"],
+      totalEarnings: "$800M",
+    },
+  };
+  
+
+  module.exports=favouritesMovies;
